a3aa94232e8e33ae3066ce94b328003e0f238954
Turbo <nicolas.pereira.dev@gmail.com>
2021-11-12 10:53:51 +0100

    [EDIT] created /product/{id} handling and POST /product now redirect to new product

--- server/router/router.go
+++ server/router/router.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -11,18 +13,20 @@ import (
 
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
+	r.StrictSlash(true)
 	r.PathPrefix("/templates/").Handler(http.StripPrefix("/templates/", http.FileServer((http.Dir("templates")))))
 	r.PathPrefix("/styles/").Handler(http.StripPrefix("/styles/", http.FileServer(http.Dir("templates/styles/"))))
 
 	r.HandleFunc("/", rootHandler).Methods("GET")
 	r.HandleFunc("/product", productHandler)
+	r.HandleFunc("/product/{id}", productByIdHandler).Methods("GET")
 	return r
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	tables, err := database.DatabaseTableCount()
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if tables > 0 {
@@ -40,17 +44,43 @@ func productHandler(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		price, err := strconv.ParseFloat(r.FormValue("price"), 64)
 		if err != nil {
-			http.Error(w, err.Error(), 500)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		id, err := database.PostProduct(r.FormValue("name"), r.FormValue("description"), price)
 		if err != nil {
-			http.Error(w, err.Error(), 500)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		fmt.Fprintf(w, "Created product with ID %d\n", id)
+		location := fmt.Sprintf("product/%d", id)
+		w.Header().Set("location", location)
+		w.WriteHeader(http.StatusSeeOther)
 	default:
 		w.Header().Set("Allow", "GET, POST")
 		http.Error(w, "405 METHOD NOT ALLOWED", http.StatusMethodNotAllowed)
 	}
+}
+
+func productByIdHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	product, err := database.GetProductById(id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "404 page not found", http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+	tmpl, err := template.ParseFiles("./templates/product.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, product)
 }
