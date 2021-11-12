32ea6321c1664ee55c2027a4574547427306c450
Turbo <turbo@module>
2021-11-12 09:12:42 +0100

    [ADD] PostProduct

--- server/database/database.go
+++ server/database/database.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/nicolas-pereira/go-ecommerce/server/config"
 
@@ -30,6 +32,46 @@ func Handler() *sql.DB {
 
 func DatabaseTableCount() (int, error) {
 	var tables int
-	err := handler.QueryRow("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA =?", config.Database().Dbname).Scan(&tables)
+	query := "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA =?"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancelfunc()
+	err := handler.QueryRowContext(ctx, query, config.Database().Dbname).Scan(&tables)
 	return tables, err
+}
+
+// Represents euro amount in cents
+type EUR int64
+
+func ToEUR(f float64) EUR {
+	return EUR((f * 100) + 0.5)
+}
+
+func (e EUR) Float64() float64 {
+	return float64(e) / 100
+}
+
+func (e EUR) Multiply(f float64) EUR {
+	v := (float64(e) * f) + 0.5
+	return EUR(v)
+}
+
+func (e EUR) String() string {
+	f := float64(e)
+	f = f / 100
+	return fmt.Sprintf("$%.2f", f)
+}
+
+// Insert product with given name, description and price, returns inserted product ID
+func PostProduct(name string, description string, price float64) (int64, error) {
+	query := "INSERT INTO product (name, description, price, fk_category_id) VALUES (?, ?, ?, 1)"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancelfunc()
+
+	res, err := handler.ExecContext(ctx, query, name, description, ToEUR(price))
+	if err != nil {
+		return -1, err
+	}
+	return res.LastInsertId()
 }
